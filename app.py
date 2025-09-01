3e4b8693c05121a8a4e928bb6e2c232b901e1820
d0x-dev <tstarkx103@gmail.com>
2025-09-01 10:57:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ CC_GENERATOR_URL = "https://drlabapis.onrender.com/api/ccgenerator?bin={}&count=
 
 #==========================BOT=======================================#
 # Bot token
-BOT_TOKEN = "8398297374:AAE-bhGRfTu5CHsF6dgrR3rzglWQ2N4KmaI"
+BOT_TOKEN = "8398297374:AAEupapcUsEr89_2GzvqbLZVcxuVYB9fOPA"
 bot = telebot.TeleBot(BOT_TOKEN)
 #=====================================================================#
 
