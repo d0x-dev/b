7a8ca4ed21c2322a54ecfdc92dad959ef9f8c81a
d0x-dev <tstarkx103@gmail.com>
2025-09-01 10:06:37 +0530

    Update b3.py

--- b3.py
+++ b3.py
@@ -46,8 +46,8 @@ def process_card_b3(fullz):
         cc, mes, ano, cvv = fullz.split("|")
 
         accounts = [
-            ("becaf11944@besaies.com", "Darkboy336@1234")
-            ("soxel63632@skateru.com", "Darkboy336@1234"),
+            ("becaf11944@besaies.com", "Darkboy336@1234"),
+            ("soxel63632@skateru.com", "Darkboy336@1234")
         ]
 
         username, password = random.choice(accounts)
